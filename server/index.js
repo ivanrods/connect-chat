43f4650f5820932154338b11397f1a393446c9d1
Ivan <contaivanrodrigues@gmail.com>
2025-11-03 13:01:12 -0300

    feat: add cors server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import User from "./models/user.js";
 import sequelize from "./config/database.js";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3333;
