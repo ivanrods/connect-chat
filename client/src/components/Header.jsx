a6edb752872d43314cc579c835302954772c1077
Ivan <contaivanrodrigues@gmail.com>
2025-11-16 10:36:05 -0300

    feat: add reader style

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,16 @@
 import { useAuth } from "../context/auth-context";
 import { MessagesSquare, LogOut } from "lucide-react";
+import styles from "../styles/Header.module.css";
 const Header = () => {
   const { signOut } = useAuth();
   return (
-    <header>
-      <MessagesSquare />
-      <h1>ConnectChat</h1>
-      <button type="button" onClick={signOut}>
+    <header className={styles.Header}>
+      <div className={styles.logo}>
+        <MessagesSquare />
+        <h1>ConnectChat</h1>
+      </div>
+
+      <button type="button" onClick={signOut} className={styles.LogOut}>
         <LogOut />
       </button>
     </header>
