b1d821b039098428d35b40a465e1371c9fd9dfc2
Ivan <contaivanrodrigues@gmail.com>
2025-11-18 10:15:04 -0300

    feat: add register page styles

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,8 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "react-router-dom";
 import { registerSchema } from "../lib/schemas/auth-schema";
+import { MessagesSquare } from "lucide-react";
+import styles from "../styles/Register.module.css";
 
 const Register = () => {
   const navigate = useNavigate();
@@ -33,27 +35,33 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <label>Nome:</label>
-        <input {...register("name")} />
-        {errors.name && <span>{errors.name.message}</span>}
-      </div>
+    <div className={styles.register}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.logo}>
+          <MessagesSquare />
+          <h1>ConnectChat</h1>
+        </div>
+        <div>
+          <label>Nome:</label>
+          <input {...register("name")} />
+          {errors.name && <span>{errors.name.message}</span>}
+        </div>
 
-      <div>
-        <label>Email:</label> <input {...register("email")} />
-        {errors.email && <span>{errors.email.message}</span>}
-      </div>
+        <div>
+          <label>Email:</label> <input {...register("email")} />
+          {errors.email && <span>{errors.email.message}</span>}
+        </div>
 
-      <div>
-        <label>Senha:</label> <input {...register("password")} />
-        {errors.password && <span>{errors.password.message}</span>}
-      </div>
-      <div>
-        <button type="submit">Criar conta</button>
-        <Link to="/login">Entrar</Link>
-      </div>
-    </form>
+        <div>
+          <label>Senha:</label> <input {...register("password")} />
+          {errors.password && <span>{errors.password.message}</span>}
+        </div>
+        <div>
+          <button type="submit">Criar conta</button>
+          <Link to="/login">Entrar</Link>
+        </div>
+      </form>
+    </div>
   );
 };
 
