b61603e568180a65bcf19126e2622841160fc31a
Ivan <contaivanrodrigues@gmail.com>
2025-11-19 10:20:36 -0300

    feat: add auto scroll to chat page

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,4 +1,5 @@
 import { useGetChat } from "../hooks/use-chat";
+import { useEffect, useRef } from "react";
 import { formatDate } from "../utils/format-date";
 import Header from "../components/Header";
 import InputMessage from "../components/inputMessage";
@@ -6,6 +7,11 @@ import styles from "../styles/Chat.module.css";
 
 const Chat = () => {
   const { messages } = useGetChat();
+  const bottomRef = useRef(null);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
 
   return (
     <div className={styles.chat}>
@@ -20,6 +26,7 @@ const Chat = () => {
             </div>
           </section>
         ))}
+        <div ref={bottomRef} />
       </div>
       <InputMessage />
     </div>
