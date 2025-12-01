e213c0a9d296475094cd23fbb6328ddabb18b1e4
Ivan <contaivanrodrigues@gmail.com>
2025-12-01 14:11:38 -0300

    feat: add multer to upload files

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { register, login } from "../controllers/auth.js";
 import { getChat, createChat } from "../controllers/chat.js";
 import { authenticateToken } from "../middlewares/authenticate-token.js";
+import { upload } from "../config/multer.js";
 
 const router = Router();
 
@@ -9,5 +10,10 @@ router.post("/register", register);
 router.post("/login", login);
 router.post("/chat", authenticateToken, createChat);
 router.get("/chat", authenticateToken, getChat);
+
+router.post("/upload", upload.single("arquivo"), (req, res) => {
+  console.log(req.file);
+  res.json({ mensagem: "Upload realizado com sucesso!", arquivo: req.file });
+});
 
 export default router;
