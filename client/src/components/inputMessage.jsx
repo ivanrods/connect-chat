ca302f7973521bfe7e7620468619233950c9ef30
Ivan <contaivanrodrigues@gmail.com>
2025-12-28 15:15:29 -0300

    add material to input message component

--- client/src/components/inputMessage.jsx
+++ client/src/components/inputMessage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { usePostChat } from "../hooks/use-chat";
 import { Send } from "lucide-react";
-import styles from "../styles/inputMessage.module.css";
+import { Box, TextField, IconButton } from "@mui/material";
 import FileUploader from "./fileUploader";
 const InputMessage = () => {
   const { postMessage } = usePostChat();
@@ -15,24 +15,57 @@ const InputMessage = () => {
     setMessage("");
   }
   return (
-    <div className={styles.inputMessage}>
-      <form onSubmit={handleSubmit}>
+    <Box
+      sx={{
+        height: 100,
+        bgcolor: "background.default",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        px: 1,
+        boxShadow: "0px -2px 6px -1px rgba(0,0,0,0.25)",
+      }}
+    >
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          width: "100%",
+          maxWidth: 750,
+          display: "flex",
+          alignItems: "center",
+          bgcolor: "background.paper",
+          borderRadius: 2,
+          px: 1,
+          "&:focus-within": {
+            border: "2px solid",
+            borderColor: "primary.main",
+          },
+        }}
+      >
         <FileUploader />
-        <input
-          type="text"
-          name="message"
-          id="message"
+
+        <TextField
+          variant="standard"
           placeholder="Digite uma mensagem"
           value={message}
-          onChange={(e) => {
-            setMessage(e.target.value);
+          onChange={(e) => setMessage(e.target.value)}
+          fullWidth
+          InputProps={{
+            disableUnderline: true,
+            sx: {
+              px: 2,
+              fontSize: 14,
+              color: "text.primary",
+            },
           }}
         />
-        <button type="submit">
-          <Send />
-        </button>
-      </form>
-    </div>
+
+        <IconButton type="submit">
+          <Send style={{ color: "var(--mui-palette-primary-main)" }} />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
