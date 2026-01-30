aefa1718b1010fa3e9f7c07d107da4c497aa9df5
Ivan <contaivanrodrigues@gmail.com>
2026-01-30 14:26:20 -0300

    feat: add data update user controller to server

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,5 @@
 import User from "../models/user.js";
+import { userSchema } from "../schemas/auth-schema.js";
 
 export const getUser = async (req, res) => {
   const { id } = req.params;
@@ -27,5 +28,37 @@ export const getUser = async (req, res) => {
     return res.status(500).json({
       message: "Erro interno ao buscar usuário",
     });
+  }
+};
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({
+      message: "ID do usuário é obrigatório",
+    });
+  }
+
+  try {
+    const { name, email } = userSchema.parse(req.body);
+
+    const user = await User.findByPk(id);
+    if (!user) {
+      return res.status(404).json({ error: "Usuário não encontrado" });
+    }
+
+    await user.update({ name, email });
+
+    return res.json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      avatar: user.avatar,
+      createdAt: user.createdAt,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Erro ao atualizar usuário" });
   }
 };
--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUser } from "../controllers/user.js";
+import { getUser, updateUser } from "../controllers/user.js";
 import { authenticateToken } from "../middlewares/authenticate-token.js";
 
 const router = Router();
 
 router.get("/:id", authenticateToken, getUser);
+router.put("/:id", authenticateToken, updateUser);
 
 export default router;
